451df5ebce2aac0803189ca242b9bea67ac00cbb
Murtaza Munaim <mmunaim@squareup.com>
2019-09-29 02:18:07 -0700

    things are working and gradescope works

--- spell.c
+++ spell.c
@@ -1,13 +1,193 @@
 #include "dictionary.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void iterate_hashtable(hashmap_t hashtable[]);
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
+bool check_word(const char* word, hashmap_t hashtable[]);
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
+int strip_nl(char* word);
+
+
+void lower_case(char* word) {
+	for (int i = 0; i < strlen(word); i++) {
+		word[i] = tolower(word[i]);
+	}
+}
+
+char* remove_punctuation(char* word) {
+	int start = 0, word_len = strlen(word);
+	while (start < word_len) {
+		if (ispunct(word[start])) {
+			start++;
+		}
+		else {
+			break;
+		}
+	}
+
+	if (start == word_len) {
+		return NULL;
+	}
+
+	for (int i = strlen(word+start)-1; i > start; i--) {
+		if (ispunct(word[i])) {
+			word[i] = 0;
+		}
+		else {
+			break;
+		}
+	}
+	return (word+start);
+}
 
 int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
-	return 0;
+	int num_misspelled = 0;
+	char *line = NULL, *tmp_str = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	int i = 0;
+	char* x_misspelled = (char*)misspelled;
+
+	while ((nread = getline(&line, &len, fp)) != -1) {
+		i = 0;
+		char *token = strtok(line, " ");
+	    while (token) {
+			strip_nl(token);
+			token = remove_punctuation(token);
+			if (strlen(token) > LENGTH) {
+				token = strtok(NULL, " ");
+				continue;
+			}
+			if (!check_word(token, hashtable)) {
+				misspelled[num_misspelled] = calloc(strlen(token), 1);
+				strncpy(misspelled[num_misspelled], token, strlen(token));
+				num_misspelled++;
+			}
+	        token = strtok(NULL, " ");
+	    }
+    }
+    free(line);
+    return num_misspelled;
 }
 
 bool check_word(const char* word, hashmap_t hashtable[]) {
+	int bucket = hash_function(word);
+	hashmap_t cursor = hashtable[bucket];
+	int word_len = strlen(word);
+	while (cursor != NULL) {
+		if (!strncmp(word, cursor->word, word_len)) {
+			return true;
+		}
+		cursor = cursor->next;
+	}
+	char *tmp = calloc(word_len, 1);
+	strncpy(tmp, word, word_len);
+	lower_case(tmp);
+	bucket = hash_function(tmp);
+	cursor = hashtable[bucket];
+	while (cursor != NULL) {
+		if (!strncmp(tmp, cursor->word, word_len)) {
+			free(tmp);
+			return true;
+		}
+		cursor = cursor->next;
+	}
+
+	free(tmp);
 	return false;
 }
 
+int strip_nl(char* word) {
+	int word_len = strlen(word);
+	for (int i = 0; i < word_len; i++) {
+		if (word[i] == '\n') {
+			word[i] = 0;
+			return i;
+		}
+	}
+	return word_len;
+}
+
+int find_node_length(hashmap_t node) {
+	hashmap_t tmp_node = node;
+	int node_count = 0;
+	while (tmp_node) {
+		node_count++;
+		tmp_node = tmp_node->next;
+	}
+	return node_count;
+}
+
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	hashmap_t new_node = NULL, tmp_node = NULL;
+	int bucket = 0;
+
+	for (int i = 0; i < HASH_SIZE; i++) {
+		hashtable[i] = NULL;
+	}
+
+	stream = fopen(dictionary_file, "r");
+
+	if (stream == NULL) {
+		perror("fopen failed the file does not exist");
+		return false;
+    }
+
+    while ((nread = getline(&line, &len, stream)) != -1) {
+		len = strip_nl(line);
+		if (len > LENGTH) {
+			len = LENGTH;
+		}
+        new_node = calloc(sizeof(node), 1);
+        new_node->next = NULL;
+        strncpy(new_node->word, line, len);
+        bucket = hash_function(line);
+        if (hashtable[bucket] == NULL) {
+			hashtable[bucket] = new_node;
+        }
+        else {
+			tmp_node = hashtable[bucket];
+			while (tmp_node->next) {
+				tmp_node = tmp_node->next;
+			}
+			tmp_node->next = new_node;
+		}
+    }
+    free(line);
+    fclose(stream);
 	return false;
+}
+
+void iterate_hashtable(hashmap_t hashtable[]) {
+	for (int i = 0; i < HASH_SIZE; i++) {
+		if (hashtable[i]) {
+			hashmap_t curr_node = hashtable[i];
+			while (curr_node) {
+				//printf("%d %s\n", i, curr_node->word);
+				curr_node = curr_node->next;
+			}
+		}
+	}
+}
+
+void free_hashtable(hashmap_t hashtable[]) {
+	hashmap_t tmp;
+	for (int i = 0; i < HASH_SIZE; i++) {
+		if (hashtable[i]) {
+			hashmap_t curr_node = hashtable[i];
+			while (curr_node) {
+				tmp = curr_node->next;
+				free(curr_node);
+				//printf("%d %s\n", i, curr_node->word);
+				curr_node = tmp;
+			}
+		}
+	}
 }
--- test.c
+++ test.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dictionary.h"
+#include "spell.h"
+
+extern hashmap_t hashtable[HASH_SIZE];
+char *misspelled[MAX_MISSPELLED] = {0};
 
 //  ./spell_check a_tale_of_two_cities.txt wordlist.txt
 int main(int argc, char** argv) {
-	printf("hello world\n");
+	int misspelled_count = 0;
+	FILE* stream = NULL;
+	if (argc != 3) {
+		printf("%s [file to check spelling] [dictionary]\n", argv[0]);
+		return 0;
+	}
+	load_dictionary(argv[2], hashtable);
+	iterate_hashtable(hashtable);
+
+	stream = fopen(argv[1], "r");
+	if (stream == NULL) {
+		perror("fopen failed the file does not exist");
+		return false;
+	}
+	misspelled_count = check_words(stream, hashtable, misspelled);
+	for (int i = 0; i < misspelled_count; i++) {
+		//printf("misspelled word: %s\n", misspelled[i]);
+		free(misspelled[i]);
+	}
+	free_hashtable(hashtable);
 	return 0;
 }
